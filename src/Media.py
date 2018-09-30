d73585c53e46ae4ab9f40e3c2ff1f4b932241664
GrayCentipede <maucarrui@gmail.com>
2018-09-29 20:32:15 -0500

    Media's empty methods now are implemented

--- src/Media.py
+++ src/Media.py
@@ -1,2 +1,49 @@
 class Media(object):
-    pass
+
+    title = None
+    artists = None
+    album = None
+    genres = None
+    length = None
+
+    def __init__(self):
+        pass
+
+    def load(self, filename):
+        pass
+
+    def set_title(self, new_title):
+        pass
+
+    def get_title(self):
+        pass
+
+    def delete_title(self, title):
+        pass
+
+    def add_artist(self, new_artist):
+        pass
+
+    def get_artists(self):
+        pass
+
+    def delete_artist(self, artist):
+        pass
+
+    def set_album(self, new_album):
+        pass
+
+    def get_album(self):
+        pass
+
+    def delete_album(self, album):
+        pass
+
+    def add_genre(self, new_genre):
+        pass
+
+    def get_genres(self):
+        pass
+
+    def delete_genre(self, genre):
+        pass
