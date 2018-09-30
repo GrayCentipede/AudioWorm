dedbcf5fb7af891aa863e63bc2edbc3cd30fb2c0
GrayCentipede <maucarrui@gmail.com>
2018-09-29 23:27:10 -0500

    QueryCompiler now has empty methods

--- src/QueryCompiler.py
+++ src/QueryCompiler.py
@@ -1,2 +1,27 @@
 class QueryCompiler(object):
-    pass
+
+    artists = None
+    songs = None
+    albums = None
+    persons = None
+
+    def __init__(self):
+        pass
+
+    def get_artists(self):
+        pass
+
+    def get_songs(self):
+        pass
+
+    def get_albums(self):
+        pass
+
+    def get_persons(self):
+        pass
+
+    def compile(self, search_string):
+        pass
+
+    def get_query(self):
+        pass
