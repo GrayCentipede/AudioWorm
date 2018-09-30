a6cd85611bf5f30fea5240a07f4aa0b36682d921
GrayCentipede <maucarrui@gmail.com>
2018-09-29 21:59:16 -0500

    Media's methods now passes the unit tests

--- src/Media.py
+++ src/Media.py
@@ -1,3 +1,8 @@
+from mutagen.mp3 import EasyMP3 as MP3
+from mutagen import MutagenError
+
+from math import floor
+
 class Media(object):
 
     title = None
@@ -5,45 +10,95 @@ class Media(object):
     album = None
     genres = None
     length = None
+    tags = None
 
     def __init__(self):
-        pass
+        self.title = 'Unknown Song'
+        self.artists = ['Unknown Artist']
+        self.album = 'Unkown Album'
+        self.genres = ['Unknown Genre']
+        self.length = ''
 
     def load(self, filename):
-        pass
+        try:
+            tags = MP3(filename)
+
+            if ('title' in tags):
+                self.set_title(tags['title'][0])
+            if ('artist' in tags):
+                for artist in tags['artist']:
+                    self.add_artist(artist)
+            if ('album' in tags):
+                self.set_album(tags['album'][0])
+            if ('genre' in tags):
+                for genre in tags['genre']:
+                    self.add_genre(genre)
+
+            self.set_length(tags.info.length)
+
+        except MutagenError:
+            raise FileNotFoundError('File not found: ' + str(filename))
 
     def set_title(self, new_title):
-        pass
+        self.title = new_title
 
     def get_title(self):
-        pass
+        return self.title
 
-    def delete_title(self, title):
-        pass
+    def delete_title(self):
+        self.title = 'Unknown Song'
 
     def add_artist(self, new_artist):
-        pass
+        if ('Unknown Artist' in self.artists):
+            self.artists.pop()
+            self.artists.append(new_artist)
+        else:
+            self.artists.append(new_artist)
 
     def get_artists(self):
-        pass
+        return self.artists
 
     def delete_artist(self, artist):
-        pass
+        try:
+            self.artists.remove(artist)
+            if (len(self.artists) == 0):
+                self.artists.append('Unknown Artist')
+        except ValueError:
+            raise ValueError('Artist not found in media file: ' + str(artist))
 
     def set_album(self, new_album):
-        pass
+        self.album = new_album
 
     def get_album(self):
-        pass
+        return self.album
 
-    def delete_album(self, album):
-        pass
+    def delete_album(self):
+        self.album = 'Unknown Album'
 
     def add_genre(self, new_genre):
-        pass
+        if ('Unknown Genre' in self.genres):
+            self.genres.pop()
+            self.genres.append(new_genre)
+        else:
+            self.genres.append(new_genre)
 
     def get_genres(self):
-        pass
+        return self.genres
 
     def delete_genre(self, genre):
-        pass
+        try:
+            self.genres.remove(genre)
+            if (len(self.genres) == 0):
+                self.genres.append('Unknown Genre')
+        except ValueError:
+            raise ValueError('Genre not found in media file: ' + str(genre))
+
+    def set_length(self, seconds):
+        minute_w_sec = seconds / 60
+        minute = floor(minute_w_sec)
+        decimals = minute_w_sec % 1
+        seconds = floor(decimals * 60)
+        self.length = str(minute) + ':' + str(seconds)
+
+    def get_length(self):
+        return self.length
