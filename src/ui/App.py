e15603497bf723aacd7960006e2762b50d1eccb1
GrayCentipede <maucarrui@gmail.com>
2018-10-14 01:35:23 -0500

    Now able to show songs and play them

--- src/ui/App.py
+++ src/ui/App.py
@@ -3,14 +3,25 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GdkPixbuf
 from gi.repository.GdkPixbuf import Pixbuf
 
+from ..MinerController import MinerController
+from ..Player import Player
+
 from .EditWindow import EditWindow
 from .SearchWindow import SearchWindow
 
-songs_list = [(str(i), str(i), str(i), str(i), str(i), str(i)) for i in range(100)]
-
 class App(Gtk.Window):
 
+    miner_controller = None
+    selected_song = None
+    player = None
+    playing = False
+    changed = False
+
     def __init__(self):
+        self.miner_controller = MinerController(self)
+        self.selected_song = None
+        self.player = Player()
+
         Gtk.Window.__init__(self, title="AudioWorm")
         self.set_default_size(1200, 720)
         self.set_border_width(10)
@@ -23,10 +34,10 @@ class App(Gtk.Window):
         self.grid.set_column_spacing(4)
         self.add(self.grid)
 
+        self.spinner = Gtk.Spinner()
+
         #Creating the ListStore model
-        self.songs_liststore = Gtk.ListStore(str, str, str, str, str, str)
-        for software_ref in songs_list:
-            self.songs_liststore.append(list(software_ref))
+        self.songs_liststore = Gtk.ListStore(str, str, str, str, str, str, str)
 
         #creating the treeview, making it use the filter as a model, and adding the columns
         self.treeview = Gtk.TreeView.new_with_model(self.songs_liststore)
@@ -38,12 +49,14 @@ class App(Gtk.Window):
 
         #creating buttons to filter by programming language, and setting up their events
         self.buttons = list()
-        for options in ["Edit", "Play", "Next"]:
+        for options in ["Edit", "Play", "Stop"]:
             button = Gtk.Button(options)
             button.set_sensitive(False)
             self.buttons.append(button)
 
         self.buttons[0].connect('clicked', self.open_edit_window)
+        self.buttons[1].connect('clicked', self.play_song)
+        self.buttons[2].connect('clicked', self.stop_song)
 
         #setting up the layout, putting the treeview in a scrollwindow, and the buttons in a row
 
@@ -81,14 +94,23 @@ class App(Gtk.Window):
         self.grid.attach_next_to(self.search, self.album_image, Gtk.PositionType.RIGHT, 1, 1)
 
         self.load = Gtk.Button('Load Music')
+        self.load.connect('clicked', self.load_database)
         self.grid.attach_next_to(self.load, self.search, Gtk.PositionType.RIGHT, 1, 1)
 
         self.grid.attach_next_to(self.scrollable_treelist, self.buttons[0], Gtk.PositionType.BOTTOM, 13, 12)
         self.scrollable_treelist.add(self.treeview)
 
+        self.grid.attach_next_to(self.spinner, self.scrollable_treelist, Gtk.PositionType.BOTTOM, 1, 1)
+
         select = self.treeview.get_selection()
         select.connect("changed", self.show_data)
 
+    def load_database(self, widget):
+        self.spinner.start()
+        self.miner_controller.load_miner()
+        self.miner_controller.add_rows()
+        self.spinner.stop()
+        self.load.set_sensitive(False)
 
     def open_search_window(self, win):
         subw = SearchWindow()
@@ -96,12 +118,45 @@ class App(Gtk.Window):
     def open_edit_window(self, win):
         sube = EditWindow()
 
+    def play_song(self, widget):
+        if (self.selected_song is not None):
+            if (not self.playing or self.changed):
+                if (self.changed):
+                    self.player.stop()
+                    self.player.load(self.selected_song[-1])
+                    self.label_1.set_text("Song: "+ self.selected_song[0] +" \n"
+                                          "Performer: "+ self.selected_song[1] +" \n")
+                    self.label_2.set_text("Album: "+ self.selected_song[2] +" \n"
+                                          "Year: "+ self.selected_song[4] +" \n")
+
+                self.player.play()
+                self.buttons[1].set_label('Pause')
+                self.playing = True
+                self.changed = False
+            else:
+                self.playing = False
+                self.buttons[1].set_label('Play')
+                self.player.pause()
+
+    def stop_song(self, widget):
+        self.player.stop()
+        self.playing = False
+        self.changed = True
+        self.buttons[1].set_label('Play')
+        self.label_1.set_text("Song: ... \n"
+                              "Performer: ... \n")
+        self.label_2.set_text("Album: ... \n"
+                              "Year: ... \n")
+
+
     def show_data(self, selection):
         model, treeiter = selection.get_selected()
         if treeiter is not None:
             for button in self.buttons:
                 button.set_sensitive(True)
-            print("You selected", model[treeiter][0])
+            self.selected_song = model[treeiter]
+            self.buttons[1].set_label('Play')
+            self.changed = True
 
 
 win = App()
